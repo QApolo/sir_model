d56745ac9710bd33e856cab0b8c77ed0acb0b4af
Juan Carlos <carlos_isc7@outlook.com>
2020-07-04 10:56:08 -0500

    Updates

---
+++ precache-manifest.7acc2f07306f25982566559e05fe4831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4c1db925d5f4b4c6bcc08525d73495",
+    "url": "/sir_model/index.html"
+  },
+  {
+    "revision": "a0fd2780721a1b012fca",
+    "url": "/sir_model/static/css/main.828bfe27.chunk.css"
+  },
+  {
+    "revision": "95e5f72cad61dbe99054",
+    "url": "/sir_model/static/js/2.54e5bd55.chunk.js"
+  },
+  {
+    "revision": "28bcb252a64156eb2fda27866b95e924",
+    "url": "/sir_model/static/js/2.54e5bd55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fd2780721a1b012fca",
+    "url": "/sir_model/static/js/main.45df48f7.chunk.js"
+  },
+  {
+    "revision": "cf758a9af1ee0d5fb773",
+    "url": "/sir_model/static/js/runtime-main.3339feaf.js"
+  },
+  {
+    "revision": "70987b9a7719c12de1c82bbbffbf7b77",
+    "url": "/sir_model/static/media/pause.70987b9a.png"
+  }
+]);
